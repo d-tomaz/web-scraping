808a958db39189cf041164638704c70775f1cd92
Daniel Tomaz <49313643+d-tomaz@users.noreply.github.com>
2023-05-22 13:40:09 -0300

    Find links to Annexes I to IV

--- web_scraping_annexes.py
+++ web_scraping_annexes.py
@@ -5,3 +5,9 @@ import zipfile
 url = "https://www.gov.br/ans/pt-br/assuntos/consumidor/o-que-o-seu-plano-de-saude-deve-cobrir-1/o-que-e-o-rol-de-procedimentos-e-evento-em-saude"
 response = requests.get(url)
 soup = BeautifulSoup(response.content, "html.parser")
+
+links = []
+for link in soup.find_all("a"):
+    href = link.get("href")
+    if href and "Anexo" in href:
+        links.append(href)
