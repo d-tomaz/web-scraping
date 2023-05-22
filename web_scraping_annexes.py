f412e02574aa0eca9a1539e9188f3d9170922054
Daniel Tomaz <49313643+d-tomaz@users.noreply.github.com>
2023-05-22 13:39:23 -0300

    Parse HTML content using BeautifulSoup

--- web_scraping_annexes.py
+++ web_scraping_annexes.py
@@ -4,3 +4,4 @@ import zipfile
 
 url = "https://www.gov.br/ans/pt-br/assuntos/consumidor/o-que-o-seu-plano-de-saude-deve-cobrir-1/o-que-e-o-rol-de-procedimentos-e-evento-em-saude"
 response = requests.get(url)
+soup = BeautifulSoup(response.content, "html.parser")
