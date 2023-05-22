c8094facc35a6ee94e794c22371b0327753c9594
Daniel Tomaz <49313643+d-tomaz@users.noreply.github.com>
2023-05-22 13:38:32 -0300

    Send GET request to retrieve HTML content

--- web_scraping_annexes.py
+++ web_scraping_annexes.py
@@ -3,3 +3,4 @@ from bs4 import BeautifulSoup
 import zipfile
 
 url = "https://www.gov.br/ans/pt-br/assuntos/consumidor/o-que-o-seu-plano-de-saude-deve-cobrir-1/o-que-e-o-rol-de-procedimentos-e-evento-em-saude"
+response = requests.get(url)
