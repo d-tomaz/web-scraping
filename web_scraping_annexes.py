95aa83f5fa9ccae4ecd80ac4c2b1cf3afeb9744c
Daniel Tomaz <49313643+d-tomaz@users.noreply.github.com>
2023-05-25 13:08:39 -0300

    Edit code documentation

--- web_scraping_annexes.py
+++ web_scraping_annexes.py
@@ -4,7 +4,7 @@ import zipfile                            # Importa o módulo zipfile para manip
 
 url = "https://www.gov.br/ans/pt-br/assuntos/consumidor/o-que-o-seu-plano-de-saude-deve-cobrir-1/o-que-e-o-rol-de-procedimentos-e-evento-em-saude" # Define a URL do site a ser acessado
 
-response = requests.get(url)              # Faz uma requisição GET para a URL e armazena a resposta
+response = requests.get(url)                          # Faz uma requisição GET para a URL e armazena a resposta
 soup = BeautifulSoup(response.content, "html.parser") # Cria um objeto BeautifulSoup para analisar o conteúdo HTML da resposta
 
 links = []                                # Cria uma lista vazia para armazenar os links dos anexos
@@ -14,7 +14,7 @@ for link in soup.find_all("a"):           # Itera sobre todos os elementos "a" e
         links.append(href)                # Adiciona o link à lista de links
         
 with zipfile.ZipFile("annexes.zip", "w") as zip_file: # Abre um arquivo ZIP chamado "annexes.zip" para escrita
-    for link in links:                    # Itera sobre todos os links na lista de links
-        filename = link.split("/")[-1]    # Obtém o nome do arquivo dividindo o link pelo caractere "/" e pegando o último item resultante
-        response = requests.get(link)     # Faz uma requisição GET para o link do anexo
+    for link in links:                                # Itera sobre todos os links na lista de links
+        filename = link.split("/")[-1]                # Obtém o nome do arquivo dividindo o link pelo caractere "/" e pegando o último item resultante
+        response = requests.get(link)                 # Faz uma requisição GET para o link do anexo
         zip_file.writestr(filename, response.content) # Escreve o conteúdo da resposta no arquivo ZIP com o nome do arquivo obtido
