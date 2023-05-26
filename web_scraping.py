dfa9e3addbc6e3af87522137b2e54a2c4d6c3a4c
Daniel Tomaz <49313643+d-tomaz@users.noreply.github.com>
2023-05-26 12:18:22 -0300

    Edit code documentation

--- web_scraping.py
+++ web_scraping.py
@@ -1,38 +1,35 @@
-import requests  # Importa o módulo requests para fazer requisições HTTP
-from bs4 import BeautifulSoup  # Importa a classe BeautifulSoup do módulo bs4 para analisar o HTML
-import zipfile  # Importa o módulo zipfile para manipular arquivos compactados em formato ZIP
+import requests  
+# Importa o módulo requests para fazer requisições HTTP
+from bs4 import BeautifulSoup  
+# Importa a classe BeautifulSoup do módulo bs4 para analisar o HTML
+import zipfile  
+# Importa o módulo zipfile para manipular arquivos compactados em formato ZIP
 
-# Define a URL do site a ser acessado
 url = 'https://www.gov.br/ans/pt-br/assuntos/consumidor/o-que-o-seu-plano-de-saude-deve-cobrir-1/o-que-e-o-rol-de-procedimentos-e-evento-em-saude'
-
-# Faz uma requisição GET para a URL e armazena a resposta
+# Define a URL do site a ser acessado
 response = requests.get(url)
-
-# Cria um objeto BeautifulSoup para analisar o conteúdo HTML da resposta
+# Faz uma requisição GET para a URL e armazena a resposta
 soup = BeautifulSoup(response.content, 'html.parser')
-
-# Cria uma lista vazia para armazenar os links dos anexos
+# Cria um objeto BeautifulSoup para analisar o conteúdo HTML da resposta
 links = []
+# Cria uma lista vazia para armazenar os links dos anexos
 
-# Itera sobre todos os elementos "a" encontrados no conteúdo HTML
 for link in soup.find_all('a'):
-    # Obtém o valor do atributo href do elemento "a"
+    # Itera sobre todos os elementos "a" encontrados no conteúdo HTML
     href = link.get('href')
-
-    # Verifica se o atributo href existe e se contém a substring "Anexo"
+    # Obtém o valor do atributo href do elemento "a"
     if href and 'Anexo' in href:
-        # Adiciona o link à lista de links
+        # Verifica se o atributo href existe e se contém a substring "Anexo"
         links.append(href)
+        # Adiciona o link à lista de links
 
-# Abre um arquivo ZIP chamado "anexos.zip" para escrita
 with zipfile.ZipFile('anexos.zip', 'w') as zip_file:
-    # Itera sobre todos os links na lista de links
+    # Abre um arquivo ZIP chamado "anexos.zip" para escrita
     for link in links:
-        # Obtém o nome do arquivo dividindo o link pelo caractere "/" e pegando o último item resultante
+        # Itera sobre todos os links na lista de links
         filename = link.split('/')[-1]
-
-        # Faz uma requisição GET para o link do anexo
+        # Obtém o nome do arquivo dividindo o link pelo caractere "/" e pegando o último item resultante
         response = requests.get(link)
-
-        # Escreve o conteúdo da resposta no arquivo ZIP com o nome do arquivo obtido
+        # Faz uma requisição GET para o link do anexo
         zip_file.writestr(filename, response.content)
+        # Escreve o conteúdo da resposta no arquivo ZIP com o nome do arquivo obtido
